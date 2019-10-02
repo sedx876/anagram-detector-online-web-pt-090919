346afbe121cb76a45275a86c57787961dbb16a19
Sharon Watkins <swatkins876@gmail.com>
2019-10-02 19:07:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(arr)
+    arr.keep_if { |str| word.split('').sort == str.split('').sort }
+  end 
+  
+end 
